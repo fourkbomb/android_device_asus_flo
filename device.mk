4e011482c8a6aa98a1d21590270e8871b89c9bae
Simon Shields <keepcalm444@gmail.com>
2016-02-13 23:43:40 +1100

    flo: Don't overwrite vendor packages and files
    Without this, on a clean build no blobs are included.
    
    Change-Id: I4d01bfd86e82315e52078ab9926deeb2e680b65c

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product-if-exists, vendor/asus/flo/flo-vendor.mk)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.carrier=wifi-only
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     device/asus/flo/fstab.flo:root/fstab.flo \
     device/asus/flo/init.flo.rc:root/init.flo.rc
 
